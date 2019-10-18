e975a858a2e7b53383e610e686ce977f5ea47869
Pank278 <pank24@gmx.net>
2019-10-18 17:14:34 +0200

    add server_example and client_example

--- client/client.py
+++ client/client.py
@@ -16,9 +16,9 @@ class HelloClient:
 
 
 if __name__ == '__main__':
-    #with HelloClient("opc.tcp://10.42.0.77:40840/freeopcua/server/") as client:
+    with HelloClient("opc.tcp://10.42.0.77:40840/freeopcua/server/") as client:
 
-    with HelloClient("opc.tcp://192.168.1.112:4840/freeopcua/server/") as client:
+    #with HelloClient("opc.tcp://192.168.1.112:4840/freeopcua/server/") as client:
     #with HelloClient("opc.tcp://localhost:40840/freeopcua/server/") as client:
         root = client.get_root_node()
         print("Root node is: ", root)
